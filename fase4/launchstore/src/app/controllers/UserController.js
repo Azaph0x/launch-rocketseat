9f401601db6afd94843459044409131c67f9d63b
João Gabriel <stuqzcrew@gmail.com>
2023-01-10 21:09:10 -0300

    add password compare, validates and session

--- fase4/launchstore/src/app/controllers/UserController.js
+++ fase4/launchstore/src/app/controllers/UserController.js
@@ -1,15 +1,48 @@
 const User = require('../models/User')
+const { formatCep, formatCpfCnpj } = require('../../lib/utils')
 
 module.exports = {
     registerForm(req, res) {
+
         return res.render('user/register')
     },
     async show(req, res) {
-        return res.send('Ok cadastrado!')
+        try {
+            const user = req.user
+            user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
+            user.cep = formatCep(user.cep)
+    
+            return res.render('user/index', { user })
+        } catch (error) {
+            console.log(error)
+        }
+       
     },
     async post(req, res) {
         const userId = await User.create(req.body)
 
+        req.session.userId = userId
+
         return res.redirect('/users')
+    },
+    async update(req, res) {
+        try {
+            const user = req.user
+            let { name, email, cpf_cnpj, cep, address } = req.body
+            cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
+            cep = cep.replace(/\D/g, "")
+
+            await User.update(user.id, {
+                name, email, cpf_cnpj, cep, address
+            })
+
+            return res.render('user/index', {
+                user: req.body,
+                success: "Conta atualizada com sucesso"
+            })
+        } catch (error) {
+            console.log(error)
+            return res.render('user/index', { error: "Algum erro aconteceu!"})
+        }
     }
 }
--- fase4/launchstore/src/server.js
+++ fase4/launchstore/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 const methodOverride = require('method-override')
+const session = require('./config/session')
 
 const server = express()
 const routes = require('./routes')
@@ -8,6 +9,7 @@ const routes = require('./routes')
 server.use(express.urlencoded({ extended: true }))
 server.use(express.static("public"))
 server.use(methodOverride('_method'))
+server.use(session)
 server.use(routes)
 
 server.set('view engine', 'njk')
