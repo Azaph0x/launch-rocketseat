490bf764627fad9a7eb3cf1f4a8537dda423fad5
João Gabriel <stuqzcrew@gmail.com>
2023-01-11 17:26:04 -0300

    fix error

--- fase4/launchstore/src/app/validators/user.js
+++ fase4/launchstore/src/app/validators/user.js
@@ -1,5 +1,5 @@
 const User = require('../models/User')
-const { compare } = require('compare')
+const { compare } = require('bcryptjs')
 
 function checkAllFields(body) {
     const keys = Object.keys(body);
